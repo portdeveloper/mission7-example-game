58192c5e5956ef46b9be13df0d92f8a318b4f1d3
portdeveloper <108868128+portdeveloper@users.noreply.github.com>
2025-08-16 15:32:44 +0300

    Add reminder for people to enable monad games id

--- app/components/PrivyProvider.tsx
+++ app/components/PrivyProvider.tsx
@@ -8,6 +8,8 @@ export default function Providers({ children }: { children: React.ReactNode }) {
       appId={process.env.NEXT_PUBLIC_PRIVY_APP_ID!}
       config={{
         loginMethodsAndOrder: {
+          // Don't forget to enable Monad Games ID support in:
+          // Global Wallet > Integrations > Monad Games ID (click on the slide to enable)
           primary: ["privy:cmd8euall0037le0my79qpz42"],
         },
       }}
